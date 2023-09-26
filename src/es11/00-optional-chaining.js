3af3cd525051d44f1427000631bdc3071c453015
julian-uribe47 <juribedio@hotmail.com>
2023-09-26 12:32:49 -0500

    ecma-script 11

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,10 @@
+const users = {
+    solomeo: {
+        apellido: 'Paredes'
+    },
+    Simon: {
+        apellido: 'Tolomeo'
+    }
+}
+
+console.log(users.Simon.apellido);
---
+++ src/es11/01-bigint.js
@@ -0,0 +1,4 @@
+const aBigNumber = 9182379128371923841n;
+const anotherBigNumber = BigInt(9182379128371923841n);
+console.log(aBigNumber);
+console.log(anotherBigNumber);
